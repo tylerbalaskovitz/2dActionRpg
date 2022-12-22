8ea8d14932b1f2ef3a2b771353402bce9f2f1e5d
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2022-12-21 20:57:28 -0500

    Pushing everything before sleeping

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,17 +15,23 @@ public class Player extends Entity{
 	GamePanel gp;
 	KeyHandler keyH;
 	
+	public final int screenX;
+	public final int screenY;
+	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
 		this.keyH = keyH;
+		
+		screenX = gp.screenWidth /2 - (gp.tileSize/2);
+		screenY = gp.screenHeight /2 - (gp.tileSize/2);
 		
 		setDefaultValues();
 		getPlayerImage();
 	}
 	
 	public void setDefaultValues() {
-		x = 100;
-		y = 100;
+		worldX = gp.tileSize * 23;
+		worldY = gp.tileSize * 21;
 		speed = 6;
 		direction = "down";
 	}
@@ -54,22 +60,23 @@ public class Player extends Entity{
 		
 		if (keyH.upPressed == true) {
 			direction = "up";
-			y -= speed;
+			worldY -= speed;
 		}
 		
-		else if (keyH.downPressed == true) {
+		
+		if (keyH.downPressed == true) {
 			direction = "down";
-			y += speed;
+			worldY += speed;
 		}
 		
-		else if (keyH.leftPressed == true) {
+		if (keyH.leftPressed == true) {
 			direction = "left";
-			x -= speed;
+			worldX -= speed;
 		}
 		
-		else if (keyH.rightPressed == true ) {
+		if (keyH.rightPressed == true ) {
 			direction = "right";
-			x += speed;
+			worldX += speed;
 		}
 		
 		spriteCounter++;
@@ -126,7 +133,7 @@ public class Player extends Entity{
 			break;
 		
 		}
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 	}
 	
 	
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 
@@ -16,18 +17,25 @@ public class GamePanel extends JPanel implements Runnable{
 	final int scale = 3;
 	
 	public final int tileSize = originalTileSize * scale;// 48x48 tile
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol; //768 pixels
-	final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+	public final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	
+	//World Seetings
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int worldWidth = tileSize * maxWorldCol;
+	public final int worldHeight = tileSize * maxWorldRow;
 	
 	//FPS
 	
 	int FPS = 60;
 	
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	Player player = new Player(this, keyH);
+	public Player player = new Player(this, keyH);
 	
 	
 	public GamePanel() {
@@ -127,6 +135,8 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		//Graphics 2D is a subclass and has more function than the standard Graphics class
 		Graphics2D g2 =  (Graphics2D)g;
+		
+		tileM.draw(g2);
 		
 		player.draw(g2);
 

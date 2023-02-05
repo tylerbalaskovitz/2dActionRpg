10c8a7009ba37f401a046e4defcabcb9c970f8d2
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2023-02-04 23:54:24 -0500

    Adding another monster and having it's images render from the constructor so each time the constructor is executed, ie each time it is loaded from an array, then the monster will be drawn and rendered onto the game screen

--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -1,5 +1,7 @@
 package monster;
 
+import java.util.Random;
+
 import entity.Entity;
 import main.GamePanel;
 
@@ -22,13 +24,49 @@ public class MON_GreenSlime extends Entity{
 		solidAreaDefaultX = solidArea.x;
 		solidAreaDefaultY = solidArea.y;
 		
+		getImage();
 	}
 	
 	public void getImage() {
 		
 		up1 = setup("/monster/greenslime_down_1");
 		up2 = setup("/monster/greenslime_down_");
+		down1 = setup("/monster/greenslime_down_1");
+		down2 = setup("/monster/greenslime_down_");
+		left1 = setup("/monster/greenslime_down_1");
+		left2 = setup("/monster/greenslime_down_");
+		right1 = setup("/monster/greenslime_down_1");
+		right2 = setup("/monster/greenslime_down_");
+	}
+	
+	public void setAction() {
+		//this allows us to set the behavior of the monster. This is more or less the same thing as the old man's AI.
 		
+actionLockCounter++;
+		
+		if(actionLockCounter == 120) {
+		
+		Random random = new Random();
+		int i = random.nextInt(100) + 1; //pick a number from 1 to 100;
+		
+			if (i <= 25) {
+				direction = "up";
+			}
+			
+			if (i > 25 && i <= 50 ) {
+				direction = "down";
+			}
+			
+			if (i > 50 && i <= 75) {
+				direction = "left";
+			}
+			
+			if (i > 75 && i <=100 ) {
+				direction = "right";
+			}
+			
+			actionLockCounter = 0;
+		}
 	}
 
 }

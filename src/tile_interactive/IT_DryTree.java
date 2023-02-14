8d592d0157a3d3dc0ec35d7abc36174209c5b7ed
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2023-02-14 10:46:13 -0500

    Now, the leftMostX variable takes x value from the first device, which since it starts on the left most part of the screen, renders as -1920. Then the rightMostX is the x value of the final screen, plus the screen's width minus  one in order to get an accurate rendering of the the final right most screen

--- src/tile_interactive/IT_DryTree.java
+++ src/tile_interactive/IT_DryTree.java
@@ -1,5 +1,12 @@
 package tile_interactive;
 
-public class IT_DryTree {
+import main.GamePanel;
+
+public class IT_DryTree extends InteractiveTile{
+	GamePanel gp;
+	public IT_DryTree(GamePanel gp) {
+		super(gp);
+		this.gp = gp;
+	}
 
 }

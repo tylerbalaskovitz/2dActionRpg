2d005eccb77aca721ffa08cc0274dbdf13771994
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-04-03 23:48:25 -0400

    Adding a new monster that rolls and moves quickly

--- src/data/Progress.java
+++ src/data/Progress.java
@@ -2,6 +2,10 @@ package data;
 
 public class Progress {
 
+	/*Useful for marking key events that will happen throughout the game such as important cutscenes, key dialogues that will occur
+	
+	*/
+	
 	public static boolean skeletonLordDefeated; 
 	
 }
---
+++ src/monster/MON_Roller.java
@@ -0,0 +1,100 @@
+package monster;
+
+import java.util.Random;
+
+import entity.Entity;
+import main.GamePanel;
+import object.OBJ_Coin_Bronze;
+import object.OBJ_Heart;
+import object.OBJ_ManaCrystal;
+
+public class MON_Roller extends Entity {
+
+GamePanel gp;
+	
+	public MON_Roller(GamePanel gp) {
+		super(gp);
+		
+		this.gp = gp;
+		
+		type = type_monster;
+		name = "Roller";
+		defaultSpeed = 10;
+		speed = defaultSpeed;;
+		maxLife = 1;
+		life = maxLife;
+		attack = 1;
+		defense = 0;
+		exp = 1;
+		//setting a solid area of the slime. Since it's smaller it needs to be customized.
+		
+		solidArea.x = 3;
+		solidArea.y = 18;
+		solidArea.width = 42;
+		solidArea.height = 21;
+		solidAreaDefaultX = solidArea.x;
+		solidAreaDefaultY = solidArea.y;
+		
+		getImage();
+	}
+	
+	public void getImage() {
+		
+		up1 = setup("/monster/bat_down_1", gp.tileSize, gp.tileSize);
+		up2 = setup("/monster/bat_down_2", gp.tileSize, gp.tileSize);
+		down1 = setup("/monster/bat_down_1", gp.tileSize, gp.tileSize);
+		down2 = setup("/monster/bat_down_2", gp.tileSize, gp.tileSize);
+		left1 = setup("/monster/bat_down_1", gp.tileSize, gp.tileSize);
+		left2 = setup("/monster/bat_down_2", gp.tileSize, gp.tileSize);
+		right1 = setup("/monster/bat_down_1", gp.tileSize, gp.tileSize);
+		right2 = setup("/monster/bat_down_2", gp.tileSize, gp.tileSize);
+	}
+	
+	
+	public void setAction() {
+	
+		if (onPath == true) {
+			//Check if it stops chasing the player, by its distance and the chance it will continue to chase.
+			//checkStopChasingOrNot(gp.player, 15, 100);
+			
+			//Searches for the direction to go via A* by getting the player's Column value and Row value.
+			//searchPath(getGoalCol(gp.player), getGoalRow(gp.player));
+
+			//checks whether or not it shoots a projectile
+			
+		} else {
+			
+			//Check if it starts chasing or not
+			//checkStartChasingOrNot(gp.player, 5, 100);
+			
+			getRandomDirection(10);
+		}
+	}
+	
+
+	
+	public void damageReaction() {
+		
+		actionLockCounter = 0;
+		//direction = gp.player.direction;
+		//onPath = true;
+	}
+	
+	public void checkDrop() {
+		
+		//Casting a D100 die
+		int i = new Random().nextInt(100)+1;
+		
+		//Set the monster drop
+		if (i  < 50 ) {
+			dropItem(new OBJ_Coin_Bronze(gp));
+		}
+		if (i >= 50 && i < 80) {
+			dropItem(new OBJ_Heart(gp));
+		}
+		
+		if (i >= 80 && i <100) {
+			dropItem(new OBJ_ManaCrystal(gp));
+		}
+	}
+}
